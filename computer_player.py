89aa7caa34b3757169afadab8b04a1e86eb14725
Barth Simonnot <barthelemy.simonnot@epfl.ch>
2020-10-28 11:51:41 +0100

    Amélioration 1 de computer_player.py avec ajustement de min et max.

--- computer_player.py
+++ computer_player.py
@@ -6,7 +6,7 @@ Created on Wed Oct 28 10:04:26 2020
 
 This script will try to guess the mysterious number
 prepared by guess_my_number.GuessMachine.
-It's strategy is to try middle number between min and max.
+It's strategy is to try random number between min and max.
 It'll adapt min and max values to match GuessMachine answers.
 """
 
@@ -23,3 +23,7 @@ if __name__ == '__main__':
         if result == 'found':
             print('Finished in %d attempts.' % guess_nachine.number_of_attempt)
             break
+        elif result == 'too low':
+            min = attempt + 1
+        else:
+            max = attempt - 1
